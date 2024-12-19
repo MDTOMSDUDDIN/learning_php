caf3fa71a46077c943c2030f8fee3832aa460612
MD TOMAS UDDIN <143870012+MDTOMSDUDDIN@users.noreply.github.com>
2024-12-19 10:28:00 +0600

    file add.delete ,json encode -decode

---
+++ File/DifferentWay/jsion.php
@@ -0,0 +1,36 @@
+<?php
+$fileName="D:\Learning_php\File\DifferentWay\way.txt";
+$students=array(
+    array(
+        "fname"=>"tomas",
+        "lname"=>"uddin",
+        "age"=>19,
+        "class"=>12,
+        "roll"=>11
+    ),
+    array(
+        "fname"=>"parvez",
+        "lname"=>"rohman",
+        "age"=>12,
+        "class"=>1,
+        "roll"=>1
+    ),
+    array(
+        "fname"=>"sabbir",
+        "lname"=>"hossan",
+        "age"=>18,
+        "class"=>11,
+        "roll"=>3
+    )
+
+);
+
+// $jsonencode=json_encode($students);
+// file_put_contents($fileName,$jsonencode);
+
+
+$data=file_get_contents($fileName);
+$decode=json_decode($data);  //object pass
+// $decode=json_decode($data,true);  //array pass
+print_r($decode);
+
